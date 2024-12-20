88080b4ff678f90a4720f90cf379bf15ca740311
Abhijeet Surshetwar <130867419+Abhijeetsurshetwar@users.noreply.github.com>
2024-12-20 18:42:10 +0530

    Entities ,Controllers , services,repositories
    Entities , services , repositories and controller of payment and booking is done

---
+++ DarshanAppServlets/src/main/java/com/example/demo/entities/Booking.java
@@ -0,0 +1,52 @@
+ 	package com.example.demo.entities;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "booking")
+public class Booking {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "BID")
+	int bid;
+	
+	@Column(name = "Name")
+	String name;
+	
+	@Column(name = "Age")
+	int age;
+	
+	@Column(name = "Gender")
+	String gender;
+	
+	@Column(name = "DID")
+	int did;
+	
+	@Column(name = "SID")
+	int sid;
+	
+	@Column(name = "PayID")
+	String payid;
+	
+	@Column(name = "BDate")
+	Date bdate;
+	
+	@Column(name = "BType")
+	String btype;
+
+}
