bfe109b37a93b5839036797c2b5af7b1a40dc76d
Wasique32 <mujawarwasique32@gmail.com>
2024-12-20 17:47:19 +0530

    comm

--- DarshanAppServlets/src/main/java/com/example/demo/repository/AccomodationRepo.java
+++ DarshanAppServlets/src/main/java/com/example/demo/repository/AccomodationRepo.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.Accomodation;
 @Repository
-public interface AccomodationRepo extends JpaRepository< Accomodation, Integer> {
+public interface AccomodationRepo extends JpaRepository<Accomodation, Integer> {
 
 }
