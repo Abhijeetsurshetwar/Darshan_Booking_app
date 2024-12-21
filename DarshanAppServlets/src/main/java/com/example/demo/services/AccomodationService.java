c8b0225513d181832479fd19b7963294b336506d
Wasique32 <mujawarwasique32@gmail.com>
2024-12-21 20:53:01 +0530

    Accomodationcontroller

--- DarshanAppServlets/src/main/java/com/example/demo/services/AccomodationService.java
+++ DarshanAppServlets/src/main/java/com/example/demo/services/AccomodationService.java
@@ -1,9 +1,22 @@
 package com.example.demo.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Accomodation;
+import com.example.demo.repository.AccomodationRepo;
 
 @Service
 public class AccomodationService {
+	@Autowired
+	AccomodationRepo accrepo;
+	
+	public List<Accomodation> getallaccomodation(){
+		return accrepo.findAll();
+		
+	}
 	
 
 }
