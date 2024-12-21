68931da5310ce70ca157aa8f99515a223c252e43
Abhijeet Surshetwar <130867419+Abhijeetsurshetwar@users.noreply.github.com>
2024-12-21 20:42:48 +0530

    Update SlotController.java
    removed

---
+++ DarshanAppServlets/src/main/java/com/example/demo/controllers/SlotController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Slot;
+import com.example.demo.services.SlotServices;
+
+import com.example.demo.services.SlotService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class SlotController {
+	@Autowired
+	SlotServices sser;
+	
+	@GetMapping("/getAllSlot")
+	public List<Slot> getSlots() {
+		return sser.getallslot();
+	}
+	
+
+}
