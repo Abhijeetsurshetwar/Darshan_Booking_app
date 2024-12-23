9bf0899f652f1f38e447bfed1466cc24a8d50cd5
Wasique32 <mujawarwasique32@gmail.com>
2024-12-23 22:51:55 +0530

    asad
    sdfsdasda

--- DarshanAppServlets/src/main/java/com/example/demo/services/AccomodationServices.java
+++ DarshanAppServlets/src/main/java/com/example/demo/services/AccomodationServices.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.Accomodation;
-import com.example.demo.repository.AccomodationRepo;
+import com.example.demo.repository.AccomodationRepositories;
 
 @Service
-public class AccomodationService {
+public class AccomodationServices {
 	@Autowired
-	AccomodationRepo accrepo;
+	AccomodationRepositories accrepo;
 	
 	public List<Accomodation> getallaccomodation(){
 		return accrepo.findAll();
